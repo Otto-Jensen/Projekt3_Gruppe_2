d4d5cb5cfdede62baa47ea63e156a0464e34e31b
Otto-Jensen1 <otje0002@stud.ek.dk>
2026-04-27 14:30:38 +0200

    Initial commit

---
+++ src/main/java/com/example/projekt3_gruppe_2/Projekt3Gruppe2Application.java
@@ -0,0 +1,13 @@
+package com.example.projekt3_gruppe_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projekt3Gruppe2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Projekt3Gruppe2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/projekt3_gruppe_2/Projekt3Gruppe2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projekt3_gruppe_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projekt3Gruppe2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
