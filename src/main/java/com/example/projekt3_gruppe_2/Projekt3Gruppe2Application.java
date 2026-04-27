f7b75621439325b4712d76eaba5e08af802390f7
Otto-Jensen1 <otje0002@stud.ek.dk>
2026-04-27 14:33:19 +0200

    test commit

--- src/main/java/com/example/projekt3_gruppe_2/Projekt3Gruppe2Application.java
+++ src/main/java/com/example/projekt3_gruppe_2/Projekt3Gruppe2Application.java
@@ -8,6 +8,7 @@ public class Projekt3Gruppe2Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Projekt3Gruppe2Application.class, args);
+        System.out.println("Test commit");
     }
 
 }
