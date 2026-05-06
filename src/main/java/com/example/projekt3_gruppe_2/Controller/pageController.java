20b9b76cde5f1d23b093b0618ee18809928018c0
deniz <deda0001@stud.ek.dk>
2026-05-06 10:59:29 +0200

    tilføjet dashbord til ledige biler/ Added dashboard to available cars

--- src/main/java/com/example/projekt3_gruppe_2/Controller/pageController.java
+++ src/main/java/com/example/projekt3_gruppe_2/Controller/pageController.java
@@ -1,4 +1,13 @@
 package com.example.projekt3_gruppe_2.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class pageController {
+
+    @GetMapping("/availableCars")
+    public String availableCars() {
+
+        return "availableCarsDash";
+    }
 }
+
